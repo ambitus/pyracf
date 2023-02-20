73e34cf1ed68408722c6bb13e560a8a86fa0b136
Leonard Carcaramo <lcarcaramo@ibm.com>
2023-02-20 08:30:15 -0500

    Debug setup.
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ class Build(build_py):
     def run(self):
         os.chdir(f"{os.path.dirname(__file__)}/pyracf/common")
         command = (
-            "c89 -c -D_XOPEN_SOURCE_EXTENDED \\ "
+            "pwd && c89 -c -D_XOPEN_SOURCE_EXTENDED \\ "
             + "     -Wc,lp64,langlvl\(extended\),STACKPROTECT\(ALL\) \\ "
             + "     -I../../safCommon -I irrsmo00.so irrsmo00.c "
             + "&& c89 -Wl,\"DLL,LP64,XPLINK\" -o irrsmo00.dll irrsmo00.o"
