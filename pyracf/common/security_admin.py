9e56a3ead3210c341a8bdf9501c4a2e9144ab63a
ElijahSwiftIBM <Elijah.Swift@IBM.com>
2023-03-09 14:49:15 -0500

    debug profile listing

--- pyracf/common/security_admin.py
+++ pyracf/common/security_admin.py
@@ -282,6 +282,7 @@ class SecurityAdmin:
             close_ind = []
             cln_ind = []
             for i, val in enumerate(out):
+                print(i,val,out)
                 if "(" in val and ")" not in val:
                     open_ind.append(i)
                 if ")" in val and "(" not in val:
