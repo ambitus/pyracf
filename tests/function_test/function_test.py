0b7ca20ba951a2a81f1ea4eb233c7ed73bdbc5eb
Leonard Carcaramo <lcarcaramo@ibm.com>
2023-07-28 09:51:24 -0400

    cleanup.
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- tests/function_test/function_test.py
+++ tests/function_test/function_test.py
@@ -9,7 +9,7 @@ def main():
     userid = getpass.getuser()
     profile = user_admin.extract(userid)
     json_profile = json.dumps(profile, indent=2)
-    print(f"Profile extract for userid {userid} was successful:\n\n{json_profile}\n")
+    print(f"Profile extract for userid '{userid}' was successful:\n\n{json_profile}\n")
 
 
 if __name__ == "__main__":
